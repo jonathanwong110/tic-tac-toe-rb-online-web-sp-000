37b0137cdf3ef816f9bdec180199c294cd27c48c
Jonathan Wong <jonathan.wong110@gmail.com>
2019-02-01 21:04:57 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -47,7 +47,7 @@ def turn(board)
    user_input = gets.strip
    index = input_to_index(user_input)
    if valid_move?(board, index)
-    move(board, index, character)
+    move(board, index, character = "X")
   else
     puts "try again"
     turn(board)
